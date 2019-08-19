b1673c520e91e5c549f0de17cc576ab233aa2921
Alek <alekaker@outlook.com>
2019-08-19 16:46:27 -0600

    add first fetch call

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
+import Container from './Container.js'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      people: ''
     }
   }
 
+
+  componentDidMount() {
+    Promise.all([
+      fetch('https://swapi.co/api/people/1/').then(response => response.json())
+      .then(person => this.setState({people: person}))
+    ])
+  }
+
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>WookieBoxxxxxx</h1>
         </header>
+        <Container peopleData = {this.state.people}/>
       </div>
     );
     }
-
-
 
 }
 
---
+++ src/Components/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Container.css';
+
+const Container = (props) => {
+  console.log(props)
+  return (
+          <main>
+          <h1>heyitsme a great big container</h1>
+          </main>
+       )
+}
+
+export default Container;
